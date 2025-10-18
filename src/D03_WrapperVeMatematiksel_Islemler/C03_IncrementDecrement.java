577eaaf0d608a95c1ddbae8b758ed711bbc4df1b
ramazan <coolstoves@gmail.com>
2025-10-18 21:35:30 +0300

    18102025

---
+++ src/D03_WrapperVeMatematiksel_Islemler/C03_IncrementDecrement.java
@@ -0,0 +1,42 @@
+package D03_WrapperVeMatematiksel_Islemler;
+
+public class C03_IncrementDecrement {
+    public static void main(String[] args) {
+
+        /*
+
+        Toplama veya carpma yaparak bir variable’in degerini artirabiliriz.
+Increment isleminin kalici olmasi icin 3 farkli sekilde assignment yapilabilir.
+
+         */
+
+
+   int sayi=3;
+
+sayi=sayi+3;
+
+        System.out.println("sayının 19.satırdaki degeri : "+ sayi);//
+
+
+        int sayi1=13;
+
+        sayi1=sayi1*3;
+
+        System.out.println(sayi1);
+
+        sayi1*=3;
+
+        System.out.println(sayi1);
+
+        int sayi5=3;
+
+        sayi5-=1;
+
+        System.out.println(sayi5);
+
+    }
+
+
+
+
+}
