3a85a1ca74cc815d86a4b1157930beded0bd064c
ramazan <coolstoves@gmail.com>
2025-11-11 19:42:16 +0300

    194211112025

---
+++ src/NestedIfelse/C02_IndirimHesabıNestedIfElse.java
@@ -0,0 +1,54 @@
+package NestedIfelse;
+
+import java.util.Scanner;
+
+public class C02_IndirimHesabıNestedIfElse {
+    public static void main(String[] args) {
+
+        //Kullanicidan aldıgı urun adedini ve indirimsiz fiyatını alin
+        // kullaniciya musteri kartı olup olmadıgını sorun
+        // musteri kartı varsa 10 urunden fazla alırsa %20
+        //                                az alırsA %15 İNDİRİM YAPIN
+        //Musteri kartı yoksa 10 urunden fazla alırsa %15
+        // az alırsa %10 indirim yapın
+
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen aldıgınız ürün adedini belitiniz");
+        int ürünAdedi=scanner.nextInt();
+        System.out.println("Sayın müşteri üyeli kartını Varsa V yoksa Y yi tuşlayınız");
+        char kartDurumu=scanner.next().toUpperCase().charAt(0);
+        System.out.println("ürün fiyatını giriniz");
+        double urunFiyatı=scanner.nextDouble();
+
+        if (kartDurumu=='V'){
+
+            if (ürünAdedi>=10){
+
+                System.out.println(ürünAdedi*urunFiyatı*0.80);//%20 indirim ile
+
+            } else if (ürünAdedi<=10&&ürünAdedi>0) {
+
+                System.out.println(ürünAdedi*urunFiyatı*0.85);  //%15 indirim ile
+
+            }else System.out.println("ürün adedi eksi olamaz lütfen bilgilerinizi doğru giriniz..");
+
+
+        } else if (kartDurumu=='Y') {
+
+            if (ürünAdedi>=10){
+
+                System.out.println(ürünAdedi*urunFiyatı*0.85);//%15 indirim ile
+
+            } else if (ürünAdedi<=10&&ürünAdedi>0) {
+
+                System.out.println(ürünAdedi*urunFiyatı*0.90);  //%10 indirim ile
+
+            }else System.out.println("ürün adedi eksi olamaz lütfen bilgilerinizi doğru giriniz..");
+
+
+        }else System.out.println("Hatali bilgi verdiniz");
+
+
+    }
+}
---
+++ src/NestedIfelse/C03_KmHesabı.java
@@ -0,0 +1,19 @@
+package NestedIfelse;
+
+public class C03_KmHesabı {
+    public static void main(String[] args) {
+
+        /*
+
+         Kullanicidan mesafeyi kilometre olarak alin ve cevirmek istedigi birimi
+sorun, istedigi birim metre veya santimetre ise cevirip yazdirin, yoksa
+“istediginiz birim sisteme kayitli degil” yazdirin.
+
+
+
+         */
+
+
+
+    }
+}
