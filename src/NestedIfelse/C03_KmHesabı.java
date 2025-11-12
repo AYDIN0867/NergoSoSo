55c31730f169066560ea3cc19772c2f495c5eead
ramazan <coolstoves@gmail.com>
2025-11-12 20:39:41 +0300

    12122025

--- src/NestedIfelse/C03_KmHesabı.java
+++ src/NestedIfelse/C03_KmHesabı.java
@@ -1,5 +1,7 @@
 package NestedIfelse;
 
+import java.util.Scanner;
+
 public class C03_KmHesabı {
     public static void main(String[] args) {
 
@@ -9,10 +11,31 @@ public class C03_KmHesabı {
 sorun, istedigi birim metre veya santimetre ise cevirip yazdirin, yoksa
 “istediginiz birim sisteme kayitli degil” yazdirin.
 
-
-
          */
 
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lütfen aldıgınız mesafeyi km olarak giriniz");
+        int girilenKM= scanner.nextInt();
+        scanner.nextLine();//bu satırı buffer hatası almamak için koyduk
+        //buffer hatası şundan kaynaklanır...sayısal veri almayı sağlayan data türleri ;
+        //byte,short, integer ,long ve virgüllü olan float ve double bunlar kendielrinden
+        //sonra \n bırakırlar bu da alt satıra gec demektir bu nedenle kendilerinden sonra METINSEL
+        // data türü geldiğinde bu satırı otomatikmen atlatırlar yokmuş gibi davranırlar buna da
+        //buffer hatası denir...
+
+        System.out.println("lütfen çevirmek istediğiniz birimi yazınız metre için  M santimetre için Cm yazın");
+        String istenenBirim=scanner.nextLine().toUpperCase();
+
+        if (!(istenenBirim.equals("M"))&&(!(istenenBirim.equals("CM")))){
+
+            System.out.println("istediginiz birim sisteme kayitli degil");
+
+        } else if (istenenBirim.equals("M")) {
+
+            System.out.println("girdiğiniz mesafenin metre olarak karşığı" + girilenKM*1000  );
+
+        }else  System.out.println("girdiğiniz mesafenin santimetre olarak karşığı" + girilenKM*1000000);
 
 
     }
