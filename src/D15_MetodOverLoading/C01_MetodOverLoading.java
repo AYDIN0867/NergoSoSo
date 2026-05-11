d798049b57aee947d958195ddf1713e440333da3
AYDIN0867 <coolstoves@gmail.com>
2026-05-11 19:37:55 +0300

    11052026

---
+++ src/D15_MetodOverLoading/C01_MetodOverLoading.java
@@ -0,0 +1,30 @@
+package D15_MetodOverLoading;
+
+public class C01_MetodOverLoading {
+    public static void main(String[] args) {
+
+/*
+
+javada bir CLASS icerisinde aynı isim ve ayni parametrelere sahip birden falza metod kullanılabilir
+
+
+ */
+
+
+
+    }
+
+    public static void topla(int sayi1, int sayi2){
+
+        System.out.println("iki sayının toplamı "+ sayi1+sayi2);
+
+    }
+
+    public static void topla(double b,int c){
+
+        System.out.println("iki sayının toplamı "+ b+c);
+
+
+    }
+
+}
